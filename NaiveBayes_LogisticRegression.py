f2e6ba6aed9be2e8d78b0371aebda652522b6f61
Cuong Ngo <phucuongngo@gmail.com>
2019-02-24 11:28:16 -0600

    initial commit

---
+++ NaiveBayes_LogisticRegression.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 24 11:18:12 2019
+
+@author: Cuong Ngo
+netid cpn180001
+CS6375 Machine Learning -
+Naive Bayes - Logistic Regression algorithms
+for text classification problem - spam detection
+"""
+
+
