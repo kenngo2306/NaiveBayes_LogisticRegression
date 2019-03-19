37ff71562b272a18b76ea00e2a722b7d64e4f46e
Cuong Ngo <phucuongngo@gmail.com>
2019-03-19 11:42:48 -0500

    complete logistic regression algo

--- NaiveBayes_LogisticRegression.py
+++ NaiveBayes_LogisticRegression.py
@@ -13,6 +13,8 @@ import os
 import glob
 import pandas as pd
 import math
+import copy
+import random
 
 ham_train_set_path='./train/ham'
 spam_train_set_path='./train/spam'
@@ -175,26 +177,22 @@ def read_stop_words(path):
 
 
 ############# Logistic Regression Algorithm part ###################
-
+w0_string = 'w0_value'
 
 # def
 def add_new_column_2(vocab_dict, word):
     vocab_dict[word] = 0
     return vocab_dict
 # Build a list of vectors of count of word, 1 vector per training example
-def build_vectors():
+    
+def build_vectors(ham_set, spam_set):
     vectors = []
-    
-    
-    
-    n_0 = 0
-    
     # build vectoes with ham emails
-    for filename in glob.glob(os.path.join(ham_train_set_path, '*.txt')):    
+    for filename in glob.glob(os.path.join(ham_set, '*.txt')):    
         vector = [{},0]   
         # count apprearance of each word for particular training example and store it in vocab_dict 
         vocab_dict = {}
-        
+        vocab_dict[w0_string] = 1
         with open(filename, errors='ignore') as f:
             for line in f:
                 line_arr= line.strip().split(' ')
@@ -206,10 +204,11 @@ def build_vectors():
         vector[0] = vocab_dict
         vectors.append(vector)
     
-    for filename in glob.glob(os.path.join(spam_train_set_path, '*.txt')):
+    for filename in glob.glob(os.path.join(spam_set, '*.txt')):
         vector = [{},1]
         # count apprearance of each word for particular training example and store it in vocab_dict 
         vocab_dict = {}  
+        vocab_dict[w0_string] = 1
         with open(filename, errors='ignore') as f:
             for line in f:
                 line_arr= line.strip().split(' ')
@@ -225,18 +224,114 @@ def build_vectors():
 
 
 
+
 # function to initialize w with a initial value
 def initialized_w (word_vectors, initial_value):
     w_vector = {}
-    
+    w_vector[w0_string] = initial_value
     for vector in word_vectors:
         for word, count in vector[0].items():
             if word not in w_vector:
                 w_vector[word] = initial_value
     return w_vector
 
-word_vectors = build_vectors()  
-w_vector = initialized_w(word_vectors, 1)
+
+# function to calculate P(Y=1 | X,W) for each training example
+def calculate_P(w_vector, word_vectors):
+    p_vector = []
+    
+    # loop through each training example
+    for word_vector in word_vectors:
+        my_sum = 0 
+        exp_value = 0
+        p_value = 0
+        for word, value in word_vector[0].items():
+            my_sum += 0 if word not in w_vector else w_vector[word] * value
+        w0 = w_vector[w0_string] 
+        
+        try:
+            exp_value = math.exp(w0 + my_sum)
+            p_value = exp_value / (1 + exp_value)
+            
+        except OverflowError:
+            p_value = 1
+        
+        p_vector.append(p_value)
+        
+    return p_vector
+    
+
+
+def update_w_vector(w_vector, p_vector, word_vectors, learning_rate, my_lambda):
+    w_vector_updated = {}
+    
+    for w_key, w_value in w_vector.items():
+         
+        my_sum = 0
+        
+        for i, word_vector in enumerate(word_vectors):
+            my_sum += 0 if (w_key not in word_vector[0]) else (word_vector[0][w_key] *  (word_vector[1] -  p_vector[i]))
+            
+       
+        
+        w_vector_updated[w_key] = w_value + learning_rate * my_sum - learning_rate * my_lambda * w_value
+        
+    return w_vector_updated
+
+
+
+
+def logistic_regression(iterations, learning_rate, lambda_value):
+    word_vectors = build_vectors(ham_train_set_path, spam_train_set_path)  
+    w_vector = initialized_w(word_vectors, 1)
+    
+    print('accuracy before training =', calculate_accuracy(w_vector, ham_test_set_path, spam_test_set_path))
+    print('learning rate= ', learning_rate) 
+    print('lambda= ', lambda_value) 
+    
+    max_i = 0
+    max_accuracy = 0
+    
+    for i in range(iterations): 
+        
+        calculate_P(w_vector, word_vectors)
+        p_vector = calculate_P(w_vector, word_vectors)
+        w_vector = update_w_vector(w_vector, p_vector, word_vectors, learning_rate, lambda_value)
+        
+        tmp_accuracy = calculate_accuracy(w_vector, ham_test_set_path, spam_test_set_path)
+        if max_accuracy < tmp_accuracy:
+            max_accuracy = tmp_accuracy
+            max_i = i
+            
+#        print('accuracy training at ', i, '= ', calculate_accuracy(w_vector, ham_test_set_path, spam_test_set_path))
+        
+    print('accuracy max after training =', max_accuracy, ' at i=', max_i)
+
+def calculate_accuracy(w_vector, ham_set, spam_set):
+    word_vectors = build_vectors(ham_set, spam_set)
+    p_vector = calculate_P(w_vector, word_vectors)
+    total_examples = len(p_vector)
+    correct_count = 0
+    
+    predicted_y_value = 0
+    
+    for i in range(0,len(p_vector)):
+        predicted_y_value = 1 if p_vector[i] >= 0.5 else 0
+        if word_vectors[i][1] == predicted_y_value:
+            correct_count += 1
+    
+    return correct_count / total_examples    
+
+
+logistic_regression(500, 0.01, 0.003)
+
+iteration = 200
+
+for i in range (20):
+    logistic_regression(iteration, random.uniform(0.01,0.3), random.uniform(0.001, 0.03))
+
+
+print ('done')
         
     
 
---
+++ Readme.txt
@@ -0,0 +1,20 @@
+accuracy before training = 0.2719665271966527
+
+logistic_regression(300, 0.009, 0.003)
+accuracy after training = 0.893305439330544
+
+logistic_regression(200, 0.02, 0.007)
+accuracy after training = 0.9058577405857741
+
+logistic_regression(500, 0.1, 0.01)
+accuracy after training = 0.9163179916317992
+
+logistic_regression(200, 0.09, 0.001)
+accuracy after training = 0.9163179916317992
+
+logistic_regression(60, 0.02, 0.006)
+accuracy training at  50 =  0.9100418410041841
+
+learning rate=  0.0729631069609485
+lambda=  0.025939297887812688
+accuracy max after training = 0.9205020920502092  at i= 54
